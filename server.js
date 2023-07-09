ec433672bd1bd9eb181c00d8b7138340c381689e
Matt Mills <mattmillsxyz@gmail.com>
2023-07-09 18:19:28 -0500

    Configuring stuff

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
+require('dotenv').config();
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+  chromeLocation: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  chromeFlags: [
+    '--no-sandbox',
+    '--headless',
+    '--disable-gpu',
+    '--remote-debugging-port=9222',
+    '--hide-scrollbars',
+  ],
+});
 
 server.use(prerender.sendPrerenderHeader());
 server.use(prerender.browserForceRestart());
-// server.use(prerender.blockResources());
+server.use(prerender.blockResources());
 server.use(prerender.addMetaTags());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+server.use(require('prerender-aws-s3-cache'));
 
 server.start();
